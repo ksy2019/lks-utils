50e0a62492674c5af33738c3e416c89c96e8771d
ksy2019 <gtx_2048@outlook.com>
2022-06-27 22:22:48 +0800

    增加睡眠函数

--- src/common.js
+++ src/common.js
@@ -1,45 +1,53 @@
 module.exports = {
     /**
+     * @description: 同步睡眠函数
+     * @param {*} time 睡眠时间，单位ms
+     * @return {*} Promise
+     */
+    sleep(time) {
+        return new Promise(res => setTimeout(res, time))
+    },
+    /**
      * @description: 防抖： 在规定的时间内重复触发会重新计时，计时结束执行函数
      * @param {*} func =>callback function
      * @param {*} delay
      * @param {*} isImmediately 是否立即执行
      * @return {*} function
      */
-    debounce(func , delay , isImmediately){
+    debounce(func, delay, isImmediately) {
         let timer
-        if(isImmediately){
-            return function(){
-                timer ? clearTimeout(timer) : func.apply(this , arguments)
-                timer = setTimeout(()=>{
+        if (isImmediately) {
+            return function() {
+                timer ? clearTimeout(timer) : func.apply(this, arguments)
+                timer = setTimeout(() => {
                     timer = null
-                },delay)
+                }, delay)
             }
-        }else{
-            return function(){
+        } else {
+            return function() {
                 timer ? clearTimeout(timer) : null
                 timer = setTimeout(() => {
-                    func.apply(this , arguments)
+                    func.apply(this, arguments)
                 }, delay);
             }
         }
     },
- 
+
     /**
      * @description: 节流函数：每个一段时间可以执行一次
      * @param {*} func => cellback function
      * @param {*} delay
      * @return {*} function
      */
-    torottle(func , delay){
+    torottle(func, delay) {
         let isTorottle;
-        return function(){
-            if(!isTorottle){
+        return function() {
+            if (!isTorottle) {
                 isTorottle = true
-                func.apply(this , arguments)
-                setTimeout(()=>{
+                func.apply(this, arguments)
+                setTimeout(() => {
                     isTorottle = false
-                },delay)
+                }, delay)
             }
         }
     },
@@ -48,7 +56,7 @@ module.exports = {
      * @param {*} obj   目标对象
      * @return {*} Object  深拷贝输出对象
      */
-    deepCopy(obj){
+    deepCopy(obj) {
         return JSON.parse(JSON.stringify(obj))
     },
     /**
@@ -56,16 +64,16 @@ module.exports = {
      * @param {*} obj 输入对象
      * @return {*} FormData Object
      */
-    formD(obj){
+    formD(obj) {
         const formData = new URLSearchParams();
         Object.keys(obj).forEach((key) => {
-          if (obj[key] instanceof Array) {
-            obj[key].forEach((item) => {
-              formData.append(key, item);
-            });
-            return;
-          }
-          formData.append(key, obj[key]);
+            if (obj[key] instanceof Array) {
+                obj[key].forEach((item) => {
+                    formData.append(key, item);
+                });
+                return;
+            }
+            formData.append(key, obj[key]);
         });
         return formData;
     },
@@ -73,11 +81,11 @@ module.exports = {
      * @description: 生成32为GUID
      * @return {*}  String
      */
-    guid(){
-            function S4() {
-               return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-            }
-            return (S4()+S4()+""+S4()+""+S4()+""+S4()+""+S4()+S4()+S4());
+    guid() {
+        function S4() {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        }
+        return (S4() + S4() + "" + S4() + "" + S4() + "" + S4() + "" + S4() + S4() + S4());
     },
     /**
      * @description: 通过post下载文件
@@ -85,49 +93,49 @@ module.exports = {
      * @param {*} data: Object
      * @return {*} viod
      */
-     postFile(url,data){
+    postFile(url, data) {
         let myform = document.createElement('form');
-        myform.setAttribute('action',url);
-        myform.setAttribute('method','post');
-        for(let item in data){
+        myform.setAttribute('action', url);
+        myform.setAttribute('method', 'post');
+        for (let item in data) {
             let myinput = document.createElement('input')
-            myinput.setAttribute('value', data[item]);  
-            myinput.setAttribute('name', item);  
-            myform.appendChild(myinput);  
-        }  
+            myinput.setAttribute('value', data[item]);
+            myinput.setAttribute('name', item);
+            myform.appendChild(myinput);
+        }
         document.getElementsByTagName("body")[0].appendChild(myform)
         myform.submit()
         myform.remove();
     },
     //点击复制文字
-    copyT(text){ 
-            if(text==''){
-                    return false
-            }
-            let myinput = document.createElement('input') 
-            myinput.setAttribute('id', 'copyinput');  
-            document.getElementsByTagName("body")[0].appendChild(myinput)  
-            myinput.value=text
-            myinput.select(); // 选择对象 
-            document.execCommand("Copy"); // 执行浏览器复制命令
-            myinput.remove();
-            Message.success("复制成功")
+    copyT(text) {
+        if (text == '') {
+            return false
+        }
+        let myinput = document.createElement('input')
+        myinput.setAttribute('id', 'copyinput');
+        document.getElementsByTagName("body")[0].appendChild(myinput)
+        myinput.value = text
+        myinput.select(); // 选择对象 
+        document.execCommand("Copy"); // 执行浏览器复制命令
+        myinput.remove();
+        Message.success("复制成功")
     },
     /**
      * @description: 读取css变量值，作用域全局
      * @param {*} key
      * @return {*} String
      */
-    readCss(key){
-            return getComputedStyle(document.documentElement).getPropertyValue(key).trim()
+    readCss(key) {
+        return getComputedStyle(document.documentElement).getPropertyValue(key).trim()
     },
     /**
      * @description: 删除:root标签下的css变量值
      * @param {*} key
      * @return {*} viod
      */
-    delCss(key){
-            document.documentElement.style.removeProperty(key);
+    delCss(key) {
+        document.documentElement.style.removeProperty(key);
     },
     /**
      * @description: 设置:root标签下的css变量值
@@ -135,12 +143,12 @@ module.exports = {
      * @param {*} val
      * @return {*} boolean
      */
-    setCss(key,val){
-            try{
-                    document.documentElement.style.setProperty(key , val);
-                    return true
-            }catch(e){
-                    return false
-            }
+    setCss(key, val) {
+        try {
+            document.documentElement.style.setProperty(key, val);
+            return true
+        } catch (e) {
+            return false
+        }
     }
 }
